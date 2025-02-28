9f3c0735e2c1c322069cb8e9c83a32613cb4493a
Lina <wulinthu@gmail.com>
2025-02-28 16:48:06 +0000

    Release 1

---
+++ src/main/java/com/linawu/talknativebackend/constant/Prompt.java
@@ -0,0 +1,17 @@
+package com.linawu.talknativebackend.constant;
+
+public class Prompt {
+    public static final String PROMPT_SETTING = "As my English tutor GPT, you should act as an English oral tutor, " +
+            "assists me in expressing themselves more naturally " +
+            "and natively in English. Upon receiving my sentence in either Chinese " +
+            "or English, you provide a basic, directly translated expression " +
+            "and then enhances the learning experience by offering at least three " +
+            "native expressions or phrasings. These suggestions focus on UK and " +
+            "Irish English, tailored to the user's preference for cultural relevance. " +
+            "Employing a friendly and encouraging tone, you should aim to make learning " +
+            "enjoyable and supportive, boosting confidence in everyday English usage. " +
+            "You should enrich my understanding by presenting a variety of expressions, " +
+            "deepening my grasp of the linguistic subtleties in the UK and Ireland." +
+            "Always start with: Let's translate your sentence and then explore some native expressions." +
+            "Now follow is my message for you to process: ";
+}
--- src/main/java/com/linawu/talknativebackend/controller/ChatController.java
+++ src/main/java/com/linawu/talknativebackend/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.linawu.talknativebackend.controller;
 
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+import static com.linawu.talknativebackend.constant.Prompt.PROMPT_SETTING;
+
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
+
+    private final OpenAiChatModel chatModel;
+    public ChatController(OpenAiChatModel chatModel){
+        this.chatModel = chatModel;
+    }
+
     @PostMapping("/message")
     public String getChatResponse(@RequestBody Map<String, String> request){
         String userMessage = request.get("message");
-        return "Your message: " + userMessage;
+        return chatModel.call(PROMPT_SETTING + userMessage);
     }
 }
